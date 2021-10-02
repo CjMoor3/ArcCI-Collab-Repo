5024ee6e0c034364e017f51ab303fd6e037dc20d
Gavin Moore <gavinmoore5225@gmail.com>
2021-10-01 19:12:13 -0500

    Removed createGeoJson.py and replaced it with createCoco.py to create JSON objects

---
+++ lib/createCoco.py
@@ -0,0 +1,53 @@
+import json
+
+##############################################################
+## createCoco.py                                            ##
+## Converts input data into COCO formatted JSON objects     ##
+## Author: Gavin Moore                                      ##
+## 10/1/2021                                                ##
+##############################################################
+
+class createCOCO:
+    def getInfo(self, year=None, version=None, desc=None, contrib=None, url=None, dateCrea=None):
+        retInfo = {'year': year, 'version': version, 'description': desc,
+                   'contributor': contrib, 'url': url, 'date_created': dateCrea}
+        return retInfo
+    
+    def getImage(self, imgId=None, width=None, height=None, imgFileName=None, imgLic=None, flckrUrl=None, dateCap=None):
+        retImage = {'id': imgId, "width": width, "height": height, "file_name": imgFileName,
+                    "license": imgLic, "flickr_url": flckrUrl, "date_captured": dateCap}
+        return retImage
+
+    def getLicense(self, licId=None, licName=None, licUrl=None):
+        retLicense = {'id': licId, 'name': licName, 'url': licUrl}
+        return retLicense
+
+    def getAnnotation(self, anoId=None, anoImg=None, anoCatId=None, anoSeg=None, area=None, segList=None, isCrowd=None):
+        retAnnotation = {'id': anoId, 'image_id': anoImg, 'category_id': anoCatId, 'segmentation': anoSeg,
+                         'area': area,'bbox': [segList], 'iscrowd': isCrowd}
+        return retAnnotation
+
+    def getCategories(self, catId=None, catName=None, category=None):
+        retCategories = {'id': [catId], 'name': catName, 'supercategory': category}
+        return retCategories
+    
+    def toCOCO(self, fileName,
+               year=None, version=None, desc=None, contrib=None, url=None, dateCrea=None,                           ## Parameters for getInfo()
+               imgId=None, width=None, height=None, imgFileName=None, imgLic=None, flckrUrl=None, dateCap=None,     ## Parameters for getImage()
+               licId=None, licName=None, licUrl=None,                                                               ## Parameters for getLicense()
+               anoId=None, anoImg=None, anoCatId=None, anoSeg=None, area=None, segList=None, isCrowd=None,          ## Parameters for getAnnotation()
+               catId=None, catName=None, category=None                                                              ## Parameters for getCategories()
+               ):
+        
+        masterDict = {'info': self.getInfo(year, version, desc, contrib, url, dateCrea),
+                      'image': self.getImage(imgId, width, height, imgFileName, imgLic, flckrUrl, dateCap),
+                      'license': self.getLicense(licId, licName, licUrl),
+                      'annotation': self.getAnnotation(anoId, anoImg, anoCatId, anoSeg, area, segList, isCrowd),
+                      'categories': self.getCategories(catId, catName, category)}
+
+        with open(fileName, 'w') as jsonObj:
+            json.dump(masterDict, jsonObj, indent=4)
+
+    def fromCOCO(self, filePath):
+        localDictionary = json.load(filePath)
+        return localDictionary
